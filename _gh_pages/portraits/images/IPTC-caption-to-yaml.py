5911606145bd9477e7e55d54265450f313de45f2
Ross Harried <ross9387@gmail.com>
2025-06-22 11:23:48 -0500

    New portfolio photos

--- _gh_pages/portraits/images/IPTC-caption-to-yaml.py
+++ _gh_pages/portraits/images/IPTC-caption-to-yaml.py
@@ -26,7 +26,7 @@ def generate_yaml_for_page(directory_path):
 # Main function
 if __name__ == "__main__":
     # Set your image directory path here
-    directory_path = '/Users/harriedr_1/Documents/github/secondcropcreative/_gh_pages/portraits/images/2024/20240908-austin'
+    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/portraits/images/2025/20250619-noah'
     
     # Generate YAML content
     yaml_data = generate_yaml_for_page(directory_path)
