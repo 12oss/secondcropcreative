0fa98d5274a8b47ab1a1169716059e315ab21213
Ross Harried <ross9387@gmail.com>
2025-06-30 09:51:07 -0500

    Gymnastics Nationals

--- _gh_pages/sports/images/IPTC-caption-to-yaml.py
+++ _gh_pages/sports/images/IPTC-caption-to-yaml.py
@@ -26,7 +26,7 @@ def generate_yaml_for_page(directory_path):
 # Main function
 if __name__ == "__main__":
     # Set your image directory path here
-    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2025/20250621-flag-football/'
+    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2025/20250627-ymca-day3/'
     
     # Generate YAML content
     yaml_data = generate_yaml_for_page(directory_path)
