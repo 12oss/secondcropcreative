86395806733bc00de0300644eaa3a1ff111b06cd
Ross Harried <ross9387@gmail.com>
2025-09-22 08:25:38 -0500

    new sports

--- _gh_pages/sports/images/IPTC-caption-to-yaml.py
+++ _gh_pages/sports/images/IPTC-caption-to-yaml.py
@@ -26,7 +26,7 @@ def generate_yaml_for_page(directory_path):
 # Main function
 if __name__ == "__main__":
     # Set your image directory path here
-    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2025/20250905-monroe-jefferson/'
+    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2025/20250920-wisconsin-maryland/'
     
     # Generate YAML content
     yaml_data = generate_yaml_for_page(directory_path)
