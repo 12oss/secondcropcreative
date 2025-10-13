22d8b23306ef5e268c0b69930f48aa2594ca3efe
Ross Harried <ross9387@gmail.com>
2025-10-12 20:27:01 -0500

    wisconsin iowa game

--- docs/sports/images/IPTC-caption-to-yaml.py
+++ docs/sports/images/IPTC-caption-to-yaml.py
@@ -26,7 +26,7 @@ def generate_yaml_for_page(directory_path):
 # Main function
 if __name__ == "__main__":
     # Set your image directory path here
-    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2025/20251004-whitewater-lacrosse/'
+    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2025/20251011-wisconsin-iowa/'
     
     # Generate YAML content
     yaml_data = generate_yaml_for_page(directory_path)
