b1274deafde4fb74bffad6fc45f86d477b98b0bc
Ross Harried <ross9387@gmail.com>
2025-11-18 20:34:40 -0600

    New galleries and gallery grid update

--- _gh_pages/sports/images/IPTC-caption-to-yaml.py
+++ _gh_pages/sports/images/IPTC-caption-to-yaml.py
@@ -26,7 +26,7 @@ def generate_yaml_for_page(directory_path):
 # Main function
 if __name__ == "__main__":
     # Set your image directory path here
-    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2025/20251108-wisconsin-washington/'
+    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2025/20251117-wisconsin-siue/'
     
     # Generate YAML content
     yaml_data = generate_yaml_for_page(directory_path)
