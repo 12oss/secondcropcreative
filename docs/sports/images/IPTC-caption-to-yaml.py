08e426b8374788ea7d7b2a406ff9d397b29dc7e2
Ross Harried <ross9387@gmail.com>
2026-04-20 08:01:18 -0500

    new sports

--- docs/sports/images/IPTC-caption-to-yaml.py
+++ docs/sports/images/IPTC-caption-to-yaml.py
@@ -26,7 +26,7 @@ def generate_yaml_for_page(directory_path):
 # Main function
 if __name__ == "__main__":
     # Set your image directory path here
-    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2026/20260402-lovb-mad-atl/'
+    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2026/20260416-packers-colony/'
     
     # Generate YAML content
     yaml_data = generate_yaml_for_page(directory_path)
