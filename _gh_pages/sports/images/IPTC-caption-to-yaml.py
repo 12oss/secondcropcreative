168169214c5a31e2087b8be1abf214d93d963d9e
Ross Harried <ross9387@gmail.com>
2026-07-20 14:20:03 -0500

    All Green County Fair Images

--- _gh_pages/sports/images/IPTC-caption-to-yaml.py
+++ _gh_pages/sports/images/IPTC-caption-to-yaml.py
@@ -26,7 +26,7 @@ def generate_yaml_for_page(directory_path):
 # Main function
 if __name__ == "__main__":
     # Set your image directory path here
-    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2026/20260716-swira-races/'
+    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2026/20260719-demo-derby/'
     
     # Generate YAML content
     yaml_data = generate_yaml_for_page(directory_path)
