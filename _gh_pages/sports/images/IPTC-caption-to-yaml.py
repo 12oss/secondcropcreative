171b602ac0b718c8069137b026c780b666567231
Ross Harried <ross9387@gmail.com>
2026-08-03 09:57:17 -0500

    Packers Training Camp

--- _gh_pages/sports/images/IPTC-caption-to-yaml.py
+++ _gh_pages/sports/images/IPTC-caption-to-yaml.py
@@ -26,7 +26,7 @@ def generate_yaml_for_page(directory_path):
 # Main function
 if __name__ == "__main__":
     # Set your image directory path here
-    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2026/20260719-demo-derby/'
+    directory_path = '/Users/harriedr/Documents/github/secondcropcreative/_gh_pages/sports/images/2026/20260802-packers-training-camp/'
     
     # Generate YAML content
     yaml_data = generate_yaml_for_page(directory_path)
